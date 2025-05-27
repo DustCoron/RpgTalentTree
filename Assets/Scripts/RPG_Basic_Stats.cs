8ab195b6f80e22fdf0f90852f049ecd8cfb37907
DustCoron <87098169+DustCoron@users.noreply.github.com>
2025-05-28 02:45:45 +0400

    Add separate resource stats for life mana and energy shield

--- Assets/Scripts/RPG_Basic_Stats.cs
+++ Assets/Scripts/RPG_Basic_Stats.cs
@@ -6,11 +6,11 @@ public class RPG_Basic_Stats
     public int strength;
     public int dexterity;
     public int intelligence;
-    public int maximumLife;
-    public int maximumMana;
+    public RPG_LifeStats life = new RPG_LifeStats();
+    public RPG_ManaStats mana = new RPG_ManaStats();
     public int armour;
     public int evasion;
-    public int energyShield;
+    public RPG_EnergyShieldStats energyShield = new RPG_EnergyShieldStats();
 
     public static RPG_Basic_Stats operator +(RPG_Basic_Stats a, RPG_Basic_Stats b)
     {
@@ -18,18 +18,28 @@ public class RPG_Basic_Stats
         result.strength = a.strength + b.strength;
         result.dexterity = a.dexterity + b.dexterity;
         result.intelligence = a.intelligence + b.intelligence;
-        result.maximumLife = a.maximumLife + b.maximumLife;
-        result.maximumMana = a.maximumMana + b.maximumMana;
+        result.life.maximumLife = a.life.maximumLife + b.life.maximumLife;
+        result.life.regeneration = a.life.regeneration + b.life.regeneration;
+        result.life.rechargeRate = a.life.rechargeRate + b.life.rechargeRate;
+        result.life.rechargeDelay = Mathf.Max(a.life.rechargeDelay, b.life.rechargeDelay);
+
+        result.mana.maximumMana = a.mana.maximumMana + b.mana.maximumMana;
+        result.mana.regeneration = a.mana.regeneration + b.mana.regeneration;
+        result.mana.rechargeRate = a.mana.rechargeRate + b.mana.rechargeRate;
+        result.mana.rechargeDelay = Mathf.Max(a.mana.rechargeDelay, b.mana.rechargeDelay);
         result.armour = a.armour + b.armour;
         result.evasion = a.evasion + b.evasion;
-        result.energyShield = a.energyShield + b.energyShield;
+        result.energyShield.maximumEnergyShield = a.energyShield.maximumEnergyShield + b.energyShield.maximumEnergyShield;
+        result.energyShield.regeneration = a.energyShield.regeneration + b.energyShield.regeneration;
+        result.energyShield.rechargeRate = a.energyShield.rechargeRate + b.energyShield.rechargeRate;
+        result.energyShield.rechargeDelay = Mathf.Max(a.energyShield.rechargeDelay, b.energyShield.rechargeDelay);
         return result;
     }
 
     public override string ToString()
     {
         return $"STR:{strength} DEX:{dexterity} INT:{intelligence} " +
-               $"Life:{maximumLife} Mana:{maximumMana} Armour:{armour} " +
-               $"Evasion:{evasion} ES:{energyShield}";
+               $"{life} {mana} Armour:{armour} " +
+               $"Evasion:{evasion} {energyShield}";
     }
 }
